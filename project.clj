e6ec9510065c9e0927de12c8378aa031f96db7f2
klapnebojsa <klapnebojsa@gmail.com>
2016-02-09 00:38:23 +0100

    .

--- project.clj
+++ project.clj
@@ -3,4 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.6.0"]])
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+                 [uncomplicate/clojurecl "0.1.2"]]
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
--- src/clo/core.clj
+++ src/clo/core.clj
@@ -1,5 +1,8 @@
 (ns clo.core
-  (:require [uncomplicate.clojurecl
+  (:require [midje.sweet :refer :all]
+            [clojure.java.io :as io]
+            [clojure.core.async :refer [chan <!!]]
+            [uncomplicate.clojurecl
              [core :refer :all]
              [info :refer [info endian-little]]]
             [vertigo
@@ -12,35 +15,89 @@
 
 (with-release [dev (first (devices (first (platforms))))])
 
-;(def dev (first (devices (first (platforms)))))
-(let [work-sizes (work-size [1])
+(let [notifications (chan)
+      follow (register notifications)work-sizes (work-size [1])
+      work-sizes (work-size [1])
       host-msg (direct-buffer 16)
       program-source
       "__kernel void hello_kernel(__global char16 *msg) {\n    *msg = (char16)('H', 'e', 'l', 'l', 'o', ' ',
    'k', 'e', 'r', 'n', 'e', 'l', '!', '!', '!', '\\0');\n}\n"
+   ;(slurp (io/resource "examples/openclinaction/ch04/hello-kernel.cl"))
       ]
   (println "work-sizes: " work-sizes)  
   (println "host-msg: " host-msg)  
   (println "program-source: " program-source)  
   
   (try
-    (with-release [dev (first (devices (first (platforms))))
+    (facts
+      "Section 4.1, Page 69."
+      (with-release [dev (first (devices (first (platforms))))
                      ctx (context [dev])
                      cqueue (command-queue ctx dev)
                      cl-msg (cl-buffer ctx 16 :write-only)
                      prog (build-program! (program-with-source ctx [program-source]))
-                     hello-kernel (kernel prog "hello_kernel")]
-      (println "dev: " dev)
-      (println "ctx: " ctx)
-      (println "cqueue: " cqueue) 
-      (println "prog: " prog)
-      (println "hello-kernel: " hello-kernel)
+                     hello-kernel (kernel prog "hello_kernel")
+                     read-complete (event)
+                     ]
+         (println "dev: " dev)
+         (println "ctx: " ctx)
+         (println "cqueue: " cqueue)
+         (println "cl-msg: " cl-msg)      
+         (println "prog: " prog)
+         (println "hello-kernel: " hello-kernel)
+         (println "read-complete: " read-complete)
+         
+         (set-args! hello-kernel cl-msg) => hello-kernel
+         (enq-nd! cqueue hello-kernel work-sizes) => cqueue
+         (enq-read! cqueue cl-msg host-msg read-complete) => cqueue
+         (follow read-complete host-msg) => notifications
+         ;(apply str (map char
+         ;                (wrap-byte-seq int8 (byte-seq (:data (<!! notifications))))))
+        ; => "Hello kernel  11111111!!!\0"
       
-      (set-args! hello-kernel cl-msg)
-      (enq-nd! cqueue  hello-kernel work-sizes)
-      (enq-read! cqueue cl-msg host-msg)
-      (apply str (map char
-                      (wrap-byte-seq int8 (byte-seq host-msg)))))
+                  (println "KRAJ " str)
+
+         )
+      )
+    
+    #_(facts
+     "Section 4.1, Page 69."
+     ;(let [host-msg (direct-buffer 16)
+    ;      work-sizes (work-size [1])
+    ;      program-source
+    ;      (slurp (io/resource "examples/openclinaction/ch04/hello-kernel.cl"))]
+      (with-release [dev (first (devices (first (platforms))))
+                     ctx (context [dev])
+                     cqueue (command-queue ctx dev)
+                    cl-msg (cl-buffer ctx 16 :write-only)
+                     prog (build-program! (program-with-source ctx [program-source]))
+                     hello-kernel (kernel prog "hello_kernel")
+                     read-complete (event)]
+
+      (println "dev: " dev)
+       (println "ctx: " ctx)
+       (println "cqueue: " cqueue)
+       (println "cl-msg: " cl-msg)      
+       (println "prog: " prog)
+       (println "hello-kernel: " hello-kernel)         
+         
+        
+         (set-args! hello-kernel cl-msg) => hello-kernel
+         (enq-nd! cqueue hello-kernel work-sizes) => cqueue
+         (enq-read! cqueue cl-msg host-msg read-complete) => cqueue
+         (follow read-complete host-msg) => notifications
+         (apply str (map char
+                         (wrap-byte-seq int8 (byte-seq (:data (<!! notifications))))))
+         => "Hello kernel  11111111!!!\0"
+         
+        (println "KRAJ ")    
+        )
+      ;)
+    )    
+    
+    
+    
+    
     (catch Exception e (println "Greska: " (.getMessage e)))))
 
 
